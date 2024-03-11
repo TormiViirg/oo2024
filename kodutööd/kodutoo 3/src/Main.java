ebf183ae8f59c7eb6037beac388a6230f79af92a
Tormi Viirg <146323571+TormiViirg@users.noreply.github.com>
2024-03-12 00:40:17 +0200

    lisa funktsionaalsus

--- kodutööd/kodutoo 3/src/Main.java
+++ kodutööd/kodutoo 3/src/Main.java
@@ -1,27 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Sisesta andmed antud formaadis eralades need omavahel tühikutega: "+/n+"Algmudeli number, pikkus(mm), kõrgus(mm), puuliik, maksumus(ümardatud täisarvuni), valitud esindus tarneks"+/n+"Näide korrektsest sisestusest:HK-47-KO-TO-R2 1000 500 Jalakas 1200 29 Elva");
-        String sisend = scanner.nextLine();
-        // Jagame sisendi tühikute järgi
-        String[] andmed = sisend.split(" ");
+        List<Tellimus> tellimused = new ArrayList<>();
 
-        String algMudeliNumber = andmed[0];
-        int pikkus = Integer.parseInt(andmed[1]);
-        int laius = Integer.parseInt(andmed[2]);
-        String puuLiik = andmed[3];
-        int maksumus = Integer.parseInt(andmed[3]);
-        String tarneEsindus = andmed[4];
+        while (true) {
+            System.out.println("Sisesta andmed antud formaadis eralades need omavahel tühikutega: " +
+                    "\nAlgmudeli number, pikkus(mm), laius(mm), puuliik, maksumus(ümardatud täisarvuni), valitud esindus tarneks " +
+                    "\nNäide korrektsest sisestusest:HK-47-KO-TO-R2 1000 500 Jalakas 1200 Elva" +
+                    "\nSisesta 'lõpeta', et lõpetada tellimuste sisestamine.");
+            String sisend = scanner.nextLine();
 
-        Nouded nouded = new Nouded(algMudeliNumber, pikkus, laius, puuLiik);
-        Tellimus tellimus = new Tellimus(nouded, maksumus, tarneEsindus);
+            if (sisend.equalsIgnoreCase("lõpeta")) {
+                break;
+            }
 
-        // Kui on vaja muuta poe nime või teha allahindlust, võiks lisada täiendava loogika
-        // Näiteks, küsides kasutajalt, kas ta soovib neid tegevusi teha
+            String[] andmed = sisend.split(" ");
+            if (andmed.length < 6) {
+                System.out.println("Vigane sisend, palun proovi uuesti.");
+                continue;
+            }
 
-        tellimus.naitaTellimusAndmeid();
+            String algMudeliNumber = andmed[0];
+            int pikkus = Integer.parseInt(andmed[1]);
+            int laius = Integer.parseInt(andmed[2]);
+            String puuLiik = andmed[3];
+            int maksumus = Integer.parseInt(andmed[4]);
+            String tarneEsindus = andmed[5];
+
+            Nouded nouded = new Nouded(algMudeliNumber, pikkus, laius, puuLiik);
+            Tellimus tellimus = new Tellimus(nouded, maksumus, tarneEsindus);
+
+            tellimused.add(tellimus);
+        }
+
+        for (Tellimus tellimus : tellimused) {
+            tellimus.naitaTellimusAndmeid();
+        }
     }
 }
